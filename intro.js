0044d6eeb878e9521afc671a2f1cc986e0d47298
Kosit <58160027@go.buu.ac.th>
2017-11-24 09:14:55 +0700

    helloworld

---
+++ intro.js
@@ -0,0 +1,30 @@
+//var greeting = 1              //number type
+//console.log(typeof greeting)
+//console.log(greeting)
+
+//greeting ='HelloWorld'        //string type
+//console.log(typeof greeting)
+//console.log(greeting)
+
+//var isToday = true            //boolean type
+//console.log(typeof isToday)
+//console.log (isToday)
+
+//var price = 100.5             //number type
+//console.log(typeof price)
+//console.log(price)
+
+//var qty                       //no undifiled
+//console.log(typeof qty)
+//console.log(qty)
+var tmp
+
+console.log(tmp)
+
+if(true) {
+    tmp = 'Temp String'
+    console.log(tmp)
+}
+
+tmp = 0
+console.log(tmp)
